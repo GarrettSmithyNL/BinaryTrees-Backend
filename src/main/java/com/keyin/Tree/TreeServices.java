7ff4a600663dd9879638037bab42ebe8ee053451
GarrettSmithyNL <garrett_smith_30@hotmail.com>
2024-12-10 14:24:22 -0330

    Added Tree services

--- src/main/java/com/keyin/Tree/TreeServices.java
+++ src/main/java/com/keyin/Tree/TreeServices.java
@@ -1,4 +1,39 @@
 package com.keyin.Tree;
 
+import com.keyin.Input.InputServices;
+import com.keyin.Node.Node;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class TreeServices {
+  private final TreeRepository treeRepository;
+  private final InputServices inputServices;
+
+  @Autowired
+  public TreeServices(TreeRepository treeRepository, InputServices inputServices) {
+    this.treeRepository = treeRepository;
+    this.inputServices = inputServices;
+  }
+
+  public Long createTreeFromInput(Long inputId) {
+    List<Integer> inputFromDb = inputServices.getInput(inputId).getInputs();
+    Tree newTree = new Tree();
+    for(Integer value : inputFromDb) {
+      newTree.add(value);
+    }
+    newTree = treeRepository.save(newTree);
+
+    return newTree.getPostingId();
+  }
+
+  private List<Node> saveNodes(Node root) {
+    if (root != null) {
+      saveNodes(root.getLeft());
+
+      saveNodes(root.getRight());
+    }
+  }
 }
